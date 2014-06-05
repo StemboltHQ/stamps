48f83237244d0c2930a8c52c804578954f926d5c
Chris Todorov <chris@freerunningtechnologies.com>
2014-06-04 22:57:42 -0700

    Fix order of parameters in Stamp hash.
    Previously if non_delivery_option is passed an invalid SOAP message
    would be generated.

--- lib/stamps/mapping.rb
+++ lib/stamps/mapping.rb
@@ -133,8 +133,8 @@ module Stamps
       property :shipmentNotificationCompanyInSubject, :from => :notify_in_subject
       property :rotationDegrees,                      :from => :rotation
       property :printMemo,                            :from => :print_memo
-      property :ReturnImageData,                      :from => :return_image_data
       property :nonDeliveryOption,                    :from => :non_delivery_option
+      property :ReturnImageData,                      :from => :return_image_data
 
       # Maps :from to Address map
       def from=(val)
